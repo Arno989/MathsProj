82ab5938a8a17258d7bcf8bc44fc8bacb9cbb4e2
Arno Defauw <arno@delandtsheer.eu>
2020-04-13 18:20:59 +0200

    added gitignore and broke a lot of stuff trying to add logging

--- server/multithreader.py
+++ server/multithreader.py
@@ -16,7 +16,7 @@ class Movie_thread(threading.Thread):
         self.__is_connected = False
         self.host = host
         self.port = port
-        # self.init_server(host, port)                #server niet onmiddellijk initialiseren (via GUI)
+        # self.init_server() 
         self.messages_queue = messages_queue
 
     @property
@@ -32,30 +32,30 @@ class Movie_thread(threading.Thread):
         self.print_gui_message("SERVER STARTED")
 
     def close_server_socket(self):
+        # remove the socket object
         self.print_gui_message("CLOSING SERVER")
         self.serversocket.close()
         self.__is_connected = False
 
     # thread-klasse!
     def run(self):
-
         try:
             while True:
                 self.print_gui_message("waiting for clients...")
-
+                
                 # establish a connection
                 socket_to_client, addr = self.serversocket.accept()
-
                 self.print_gui_message(f"Established connection with: {addr}")
 
+                # initiate thread
                 clh = ClientHandler(socket_to_client, self.messages_queue)
                 clh.start()
 
-                self.print_gui_message(
-                    f"Current thread count: {threading.active_count()}"
-                )
+                self.print_gui_message(f"Current thread count: {threading.active_count()}")
         except Exception as ex:
+            logging.error(f"Multithreader.py: {ex}")
             self.print_gui_message("SOCKET CLOSED")
 
     def print_gui_message(self, message):
         self.messages_queue.put(f"Server:>  {message}")
+        logging.info(f"Multithreader:  {message}")
--- server/start.py
+++ server/start.py
@@ -1,20 +1,39 @@
-from tkinter import *
+import logging
+import sys
 from threading import Thread
-
-import os
-print(os.getcwd())
+from tkinter import *
 
 from server_gui import ServerWindow
 
 
-def callback():
-    print("callback")
-    gui.close()
-    root.destroy()
+def Stop():
+    logging.info("Deleting window")
+    try:
+        print("1")
+        root.destroy()
+        print("2")
+        sys.exit()  # stop code run after window deletion
+        print("3")
+        logging.info("Deleted window, this should not show up")
+        print("4")
+    except Exception as ex:
+        logging.info("Start.py: " + str(ex))
 
 
-root = Tk()
-root.geometry("600x500")
-gui = ServerWindow(root)
-root.protocol("WM_DELETE_WINDOW", callback)
-root.mainloop()
+def _destroy(event):
+    logging.info("destroy")
+
+
+try:
+    root = Tk()
+    root.geometry("400x600")
+    gui = ServerWindow(root)
+    root.protocol("WM_DELETE_WINDOW", Stop)
+except Exception as ex:
+    logging.error(f"Start.py: {ex}")
+
+try:
+    root.mainloop()
+except Exception as ex:
+    logging.error(f"Start.py: {ex}")
+    Stop()
