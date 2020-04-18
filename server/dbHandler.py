7515d3c603e6c565bd7644294df6ea450deef67d
Arno Defauw <arno@delandtsheer.eu>
2020-04-18 18:26:38 +0200

    adjusted logging for better debugging

--- server/dbHandler.py
+++ server/dbHandler.py
@@ -3,25 +3,22 @@ from pathlib import Path
 from typing import List, Union
 from data.movie import User
 
-startUsers = [User(name='yeet', username='yeet', password='hash', email='yeet'), User(name='yeet2', username='yeet2', password='hash2', email='yeet2')]
+startUsers = [
+    User(name="yeet", username="yeet", password="hash", email="yeet"),
+    User(name="yeet2", username="yeet2", password="hash2", email="yeet2"),
+]
 
 
 def obj_dict(obj):
     return obj.__dict__
 
+
 def create_file_if_not_exists(path: str):
     if not Path(path).is_file():
         with open(path, "w") as users_file:
-            print("create db")
             json.dump([ob.__dict__ for ob in startUsers], users_file)
 
 
 def get_json_file_contents(path: str):
     with open(path) as users_file:
-        #loads
-        print("opening content")
-        content = json.load(users_file)
-        #content = json.load(users_file, object_hook=User)
-        print(content)
-        
-        return content
+        return json.load(users_file)
--- server/multithreader.py
+++ server/multithreader.py
@@ -6,64 +6,65 @@ import sys
 import pandas as pd
 import numpy as np
 
-from clienthandler import ClientHandler
-
-logging.basicConfig(level=logging.INFO)
+from server.clienthandler import ClientHandler
 
 
 class Movie_thread(threading.Thread):
-    def __init__(self, host, port, messages_queue):
+    def __init__(self, host, port, message_queue):
         threading.Thread.__init__(self)
-        self.__is_connected = False
+        self.connected = False
         self.host = socket.gethostname()
         self.port = port
-        # self.init_server()
-        self.messages_queue = messages_queue
+        self.message_queue = message_queue
 
     @property
     def is_connected(self):
-        return self.__is_connected
+        return self.connected
 
     def init_server(self):
         # create a socket object
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.serversocket.bind((self.host, self.port))
         self.serversocket.listen(5)
-        self.__is_connected = True
+        self.connected = True
         self.print_gui_message("SERVER STARTED")
 
     def close_server_socket(self):
         # remove the socket object
         self.print_gui_message("CLOSING SERVER")
         self.serversocket.close()
-        self.__is_connected = False
+        self.connected = False
 
     # thread-klasse!
     def run(self):
         try:
+            threads = 0
+            clh = []
             while True:
-                self.print_gui_message("waiting for clients...")
+                self.print_gui_message("MT> waiting for client...")
 
                 # establish a connection
                 socket_to_client, addr = self.serversocket.accept()
-                self.print_gui_message(f"Established connection with: {addr}")
+                self.print_gui_message(f"MT> Established connection with: {addr}")
 
                 # initiate thread
-                clh = ClientHandler(socket_to_client, self.messages_queue)
+                threads += 1
+                clh.append(ClientHandler(socket_to_client, self.messages_queue, threads))
                 try:
-                    clh.start()
+                    clh[threads].start()
                 except (KeyboardInterrupt, SystemExit):
+                    logging.info("MT> Interrupted, calling Stop.")
                     clh.stop()
                     sys.kill()
 
                 self.print_gui_message(
-                    f"Current thread count: {threading.active_count()}"
+                    f"Current thread count: {threads}" # threading.active_count()
                 )
         except Exception as ex:
-            logging.error(f"Multithreader.py :> {ex}")
-            self.print_gui_message(f"SOCKET CLOSED: {ex}")
+            logging.error(f"MT> {ex}")
+            self.print_gui_message(f"MT Error: {ex}")
 
     def print_gui_message(self, message):
-        self.messages_queue.put(f"Server:>  {message}")
-        logging.info(f"Multithreader:  {message}")
+        self.message_queue.put(f"MT> {message}")
+        logging.info(f"MT> {message}")
 
